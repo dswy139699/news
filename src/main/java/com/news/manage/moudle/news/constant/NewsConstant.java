e75a4c76c06c072d4723b0a019b840d4a6e4564b
吴吉斌 <635730939@qq.com>
2022-04-27 01:40:52 +0800

    "文件 管理"

--- src/main/java/com/news/manage/moudle/news/constant/NewsConstant.java
+++ src/main/java/com/news/manage/moudle/news/constant/NewsConstant.java
@@ -1,6 +1,6 @@
 package com.news.manage.moudle.news.constant;
 
 public class NewsConstant {
-    public static String FILE_PATH = "/app/deploy/file/";
-    public static String IP_ADDRESS = "http://47.111.104.53";
+    public static String FILE_PATH = "/load/";
+    public static String IP_ADDRESS = "http://47.111.104.53:8888";
 }
